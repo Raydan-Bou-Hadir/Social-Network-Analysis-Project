47ef47247eead5e6bee2808e23b213ffb2346a14
Raydan Bou Hadir <raydanbouhadir00@gmail.com>
2024-07-20 18:48:37 +0300

    Implement the addUser method in the Graph class.

---
+++ graph.py
@@ -0,0 +1,21 @@
+from verify_email import verify_email
+import User
+
+class Graph:
+    def __init__(self):
+        self.users = {} #Dictionary for userId
+
+    def addUser(self, userId, name, email, age):
+        if not isinstance(name, str):
+            return 'Invalid name.'
+        
+        if not verify_email(email):
+            return
+        
+        if not isinstance(age, int) or 18 > age > 100:
+            return 'Invalid age number'
+        
+        if userId not in self.users:
+            self.users[userId] = User(userId, name, email, age)
+            return True
+        return False
