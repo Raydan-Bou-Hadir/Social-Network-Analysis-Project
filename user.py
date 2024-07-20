d5ff7760d5fea6a32a4c5562b81ba0bda8967728
Raydan Bou Hadir <raydanbouhadir00@gmail.com>
2024-07-20 18:52:42 +0300

    Implement the addFriend method in the User class.

--- user.py
+++ user.py
@@ -1,1 +1,18 @@
+class User:
+    def __init__(self, userId, name, email, age):
+        self.userId = userId
+        self.name = name
+        self.email = email
+        self.age = age
+        self.friends = set() #set to store friends for efficient lookup and management
+        self.interests = []
+        self.posts = []
+
+    def addFriend(self, friend):
+        if friend not in self.friends:
+            self.friends.add(friend)
+            print('Added Successfully.')
+        else:
+            print('Already a friend.')
+
 
