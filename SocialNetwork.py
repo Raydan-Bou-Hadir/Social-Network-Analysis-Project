6927c322299f266f49cbfe6d4e009646d13c7204
Raydan Bou Hadir <raydanbouhadir00@gmail.com>
2024-07-22 18:26:28 +0300

    Create GUI element.

--- SocialNetwork.py
+++ SocialNetwork.py
@@ -11,3 +11,16 @@ class SocialNetwork:
         self.graph = GraphFile.Graph()
         self.root = root
         self.root.title("Social Network")
+    
+        self.createWidgets()
+
+    def createWidgets(self):
+        tk.Label(self.root, text="Social Network GUI", font=("Arial", 16)).pack(pady=10)
+
+        self.addUserWidgets()
+        self.removeUserWidgets()
+        self.addRelationshipWidgets()
+        self.removeRelationshipWidgets()
+        self.viewUserDetailsWidget()
+        self.visualizeGraphWidget()
+        self.exitButton()
