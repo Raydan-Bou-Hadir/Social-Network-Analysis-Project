fc164e8f56e34667ec252b910a700667def03d81
Raydan Bou Hadir <raydanbouhadir00@gmail.com>
2024-07-22 17:57:04 +0300

    Add Social network CLI menu and choice 1(add user).

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
-from Graph import Graph
+from GraphFile import Graph
 from User import User
+
+graph = Graph()
+running = True
+
+while running:
+        print('\nSocial Network CLI')
+        print('1. Add User')
+        print('2. Remove User')
+        print('3. Add Relationship')
+        print('4. Remove Relationship')
+        print('5. Update User Profile')
+        print('6. Display User Information')
+        print('7. Display Network Statistics')
+        print('8. Visualize Graph')
+        print('0. Exit')
+        choice = input('Enter your choice: ')
+
+        if choice == '1':
+            userId = input('Enter user ID: ')
+            name = input('Enter name: ')
+            email = input('Enter email: ')
+            age = int(input('Enter age: '))
+            result = graph.addUser(userId, name, email, age)
+            if result:
+                print(result)
+            else:
+                print(result)
