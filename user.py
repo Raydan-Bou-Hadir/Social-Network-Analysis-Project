ac3feade6f031ccaf3466b862968fd7f891808ba
Raydan Bou Hadir <raydanbouhadir00@gmail.com>
2024-07-22 18:16:57 +0300

    Fix the bug in addFriend method

--- user.py
+++ user.py
@@ -1,19 +1,23 @@
+
 class User:
     def __init__(self, userId, name, email, age):
         self.userId = userId
         self.name = name
         self.email = email
         self.age = age
-        self.friends = set() #set to store friends for efficient lookup and management
+        self.friends = set()
         self.interests = []
         self.posts = []
 
     def addFriend(self, friend):
-        if friend not in self.friends:
-            self.friends.add(friend)
-            print('Added Successfully.')
+        if isinstance(friend, User):
+            if friend not in self.friends:
+                self.friends.add(friend)
+                print('Added Successfully.')
+            else:
+                print('Already a friend.')
         else:
-            print('Already a friend.')
+            print('Invalid friend object.')
 
     def unFollow(self, friend):
         if friend not in self.friends:
